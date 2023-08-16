a263f13ff565c04a18dcf3dd87c50a463bb40b5b
kyaakpa <kyaakpa@users.noreply.github.com>
2023-08-16 20:25:02 +1000

    create task success

---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,31 @@
+const Task = require("../models/Task");
+
+const getAllTasks = (req, res) => {
+  res.send("get all tasks");
+};
+
+const createTask = async (req, res) => {
+  try{
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({msg: error})
+  }
+};
+const getTask = (req, res) => {
+  res.json(req.params);
+};
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
